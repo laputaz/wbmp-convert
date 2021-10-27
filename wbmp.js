44d47c263a0a61e54980077e1bc81edf74a4f49d
river.zhuang <river.zhuang@klook.com>
2021-10-27 17:08:57 +0800

    fs

--- wbmp.js
+++ wbmp.js
@@ -4,53 +4,51 @@ export const decodeWBMP = function (arrayBuffer) {
     // 其实位
     let ptr = 0
 
-    // 读一个字节
-    // 0xff => 11111111
+    // 读一个字节 ptr++
     function readOctet() {
-        return bytes[ptr++] & 0xff
+        return bytes[ptr++]
     }
 
-    // 读取多位
-    function readMultiByteInteger() {
-        var result = 0
-        // eslint-disable-next-line
+
+    // 读取多字节整数
+    function readMultiByteInt() {
+        let result = 0
         while (true) {
-            if (result & 0xfe000000) throw 'error parsing integer'
-            var b = bytes[ptr++]
-            // 左移7位，低位7位补零
-            // b取后 7 位，补上取
-            result = (result << 7) | (b & 0x7f)
-            // 最高位是不是1，是的话继续，不是的话返回（肯定会有一个最高位的0作为多字节数结尾的字节）
-            if (!(b & 0x80)) return result
+            // 读取下一位
+            let next = bytes[ptr++]
+            // 左移7位 => 低位7位补零
+            result = result << 7
+            // next & 0b01111111 表示最高位变0，其余位置保留
+            result = result | (next & 0b01111111)
+            // 判断最高位是不是0，是的话结束。（肯定会有一个最高位为0的字节，作为多字节数结尾的字节）
+            if (!(next & 0b10000000)) {
+                return result
+            }
         }
     }
 
     try {
-        // We only support image type 0: B/W, no compression
-        if (readMultiByteInteger() != 0) return false
-        // We don't expect any extended headers here.
+        // 以 0 开始
         if (readOctet() != 0) return false
-        var width = readMultiByteInteger()
-        var height = readMultiByteInteger()
-        // Reject incorrect image dimensions.
-        if (width == 0 || width > 65535 || height == 0 || height > 65535)
-            return false
-        // Create a canvas to draw the pixels into.
-        var canvas = document.createElement('canvas')
+        // 第二位也是 0
+        if (readOctet() != 0) return false
+        let width = readMultiByteInt()
+        let height = readMultiByteInt()
+        // 建立一个 canvas
+        let canvas = document.createElement('canvas')
         canvas.setAttribute('width', width)
         canvas.setAttribute('height', height)
-        var ctx = canvas.getContext('2d')
-        var imageData = ctx.createImageData(width, height)
-        var data = imageData.data
-        // Decode the image.
-        for (var y = 0; y < height; ++y) {
-            for (var x = 0; x < width; x += 8) {
-                var bits = bytes[ptr++]
-                var w = (y * width + x) * 4
+        let ctx = canvas.getContext('2d')
+        let imageData = ctx.createImageData(width, height)
+        let data = imageData.data
+        // 设置图片数据
+        for (let y = 0; y < height; ++y) {
+            for (let x = 0; x < width; x += 8) {
+                let bits = bytes[ptr++]
+                let w = (y * width + x) * 4
                 // rgba
-                // eslint-disable-next-line
                 function write(bit) {
-                    var color = bit ? 255 : 0
+                    let color = bit ? 255 : 0
                     data[w++] = color
                     data[w++] = color
                     data[w++] = color
@@ -68,12 +66,12 @@ export const decodeWBMP = function (arrayBuffer) {
             }
         }
         if (ptr > bytes.length) return null
-        // Update the canvas pixels.
+        // 从 0 位置开始写
         ctx.putImageData(imageData, 0, 0)
-        // Convert to an image.
+        // 转换为 DataURI
         return canvas.toDataURL('image/png')
     } catch (e) {
-        // Error occured.
+        // 出错了
         return null
     }
 }
